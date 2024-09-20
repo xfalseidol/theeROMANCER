71ff016a5b61dc94c717297ba46141415772b27a
Gary Briggs <gbriggs@rand.org>
2024-09-20 10:26:31 -0700

    Don't chdir during an import

--- demo/hotline/context.py
+++ demo/hotline/context.py
@@ -1,7 +1,7 @@
 import os
 import sys
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../..')))
-os.chdir('demo/hotline')
+# os.chdir('demo/hotline')
 import context
 import casebasedreasoner
 import romancer.environment
