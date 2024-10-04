08a89db76fd3441b5c25bbeb691812463839f72c
Gary Briggs <chunky@icculus.org>
2024-10-03 23:27:41 -0700

    Re-render images after running model

--- demo/hotline/app.py
+++ demo/hotline/app.py
@@ -1,12 +1,9 @@
-from tempfile import tempdir
-import asyncio
 from shiny import App, reactive, render, ui
 from shiny.types import ImgData
 
 from hotline_demo import run_hotline
 import matplotlib.pyplot as plt
 import os
-import base64
 import tempfile
 
 img_width_shiny = 4
@@ -16,8 +13,14 @@ app_ui = ui.page_fillable(
         body {
             font-size: 12px;
         }
+        .shiny-image-output {
+            width: auto !important;
+            height: auto !important;
+        }
         .card-body img {
-            margin-bottom: 5px;
+            # margin-bottom: 5px;
+            width: auto !important;
+            height: auto !important;
         }
         .card-blue {
             background-color: lightblue;
@@ -63,7 +66,6 @@ app_ui = ui.page_fillable(
         ),
         title = "ROMANCER Hotline",
         window_title = "RAND Hotline",
-
 )
 
 )
@@ -71,7 +73,7 @@ app_ui = ui.page_fillable(
 def on_slider_change(value):
     print(f"Slider value changed to {value}")
 
-chartdir = "./chartdir"
+chartdir = "chartdir"
 if not os.path.exists(chartdir):
     os.mkdir(chartdir)
 
@@ -104,12 +106,19 @@ def server(input, output, session):
 
     chart_imgs = {}
 
-    hack_trigger = reactive.Value(0)
+    # Shiny wants the output images to reference the inputs they rely on.
+    def reference_all_inputs():
+        # Cannot find documentation on a way to do this programmatically,
+        #   or without accessing members with underscore prefixes
+        inputs = []
+        for side in ["blue", "red"]:
+            inputs.extend([f"{side}_initial_pbf", f"{side}_pbf_halflife", f"{side}_response_threshold"])
+        _ = [session.input[q]() for q in inputs]
 
     @output
     @render.image
     def blue_timeline():
-        hack_trigger()
+        reference_all_inputs()
         chart_img = chart_imgs["BLUE TIMELINE"]
         retval: ImgData = {"src": f"{chartdir}/{chart_img}"}
         return retval
@@ -117,6 +126,7 @@ def server(input, output, session):
     @output
     @render.image
     def red_timeline():
+        reference_all_inputs()
         chart_img = chart_imgs["RED TIMELINE"]
         retval: ImgData = {"src": f"{chartdir}/{chart_img}"}
         return retval
@@ -124,7 +134,7 @@ def server(input, output, session):
     @output
     @render.image
     def blue_amygdala():
-        hack_trigger()
+        reference_all_inputs()
         chart_img = chart_imgs["BLUE MOOD METER"]
         retval: ImgData = {"src": f"{chartdir}/{chart_img}"}
         return retval
@@ -132,6 +142,7 @@ def server(input, output, session):
     @output
     @render.image
     def red_amygdala():
+        reference_all_inputs()
         chart_img = chart_imgs["RED MOOD METER"]
         retval: ImgData = {"src": f"{chartdir}/{chart_img}"}
         return retval
@@ -139,6 +150,7 @@ def server(input, output, session):
     @output
     @render.image
     def blue_ladder():
+        reference_all_inputs()
         chart_img = chart_imgs["BLUE ESCALATION LADDER"]
         retval: ImgData = {"src": f"{chartdir}/{chart_img}"}
         return retval
@@ -146,6 +158,7 @@ def server(input, output, session):
     @output
     @render.image
     def red_ladder():
+        reference_all_inputs()
         chart_img = chart_imgs["RED ESCALATION LADDER"]
         retval: ImgData = {"src": f"{chartdir}/{chart_img}"}
         return retval
@@ -153,6 +166,7 @@ def server(input, output, session):
     @output
     @render.image
     def blue_resolve():
+        reference_all_inputs()
         chart_img = chart_imgs["BLUE RESOLVE"]
         retval: ImgData = {"src": f"{chartdir}/{chart_img}"}
         return retval
@@ -160,6 +174,7 @@ def server(input, output, session):
     @output
     @render.image
     def red_resolve():
+        reference_all_inputs()
         chart_img = chart_imgs["RED RESOLVE"]
         retval: ImgData = {"src": f"{chartdir}/{chart_img}"}
         return retval
@@ -174,17 +189,14 @@ def server(input, output, session):
             if len(t) > 0:
                 this_title = t
 
-        thisimg = tempfile.mktemp(".png", base64.b64encode(this_title.encode("ascii")).decode("utf-8"), chartdir)
-        print("This img: " + thisimg)
+        thisimg = tempfile.mktemp(".png", dir=chartdir)
         thisfname = os.path.basename(thisimg)
-        # thisfname = base64.b64encode(this_title.encode("ascii")).decode("utf-8") + ".png"
-        # thisimg = os.path.join(chartdir, thisfname)
         plt.savefig(thisimg, format='png')
         chart_imgs[this_title] = thisfname
 
     plt.show = matplotlib_capture
 
-app = App(app_ui, server, static_assets={"/charts": os.path.join(os.path.dirname(__file__), chartdir)})
+app = App(app_ui, server, static_assets={f"/{chartdir}": os.path.join(os.path.dirname(__file__), chartdir)})
 
 if __name__ == "__main__":
     app.run()
